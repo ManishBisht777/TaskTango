f590d02e002a11502f5f074e3dce6ff6981ba541
manishbisht <manishbisht9711@gmail.com>
2023-06-14 18:06:19 +0530

    feat: sprint 1 done

--- lib/validations/task.ts
+++ lib/validations/task.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const taskSchema = z.object({
   title: z.string().min(3).max(128),
   description: z.string(),
-  dueDate: z.date(),
-  status: z.string(),
+  dueDate: z.string().nonempty(),
+  status: z.string().nonempty(),
+  priority: z.string().nonempty(),
 });
